84d86e657332d44cbadd09a6c3b67247dfffa225
sho naito <sho20100@gmail.com>
2015-06-08 20:22:57 +0900

    add gitconfig and ignore

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+ln -s ~/dotfiles/dot ~/.gitconfig
+ln -s ~/dotfiles/dot.gitignore.global ~/.gitignore.global
+
+case "${OSTYPE}" in
+darwin*)
+  cp ~/dotfiles/dot.gitconfig_mac  ~/.gitconfig.local 
+  ;;
+linux*)
+  ;;
+esac
+
