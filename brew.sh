5e6852ccb34ccc2466561f88516cdc7343a8c92a
snaitou <snaitou@snaitou.local>
2015-06-08 17:50:03 +0900

    add packages

--- brew.sh
+++ brew.sh
@@ -3,13 +3,27 @@
 # Make sure using latest Homebrew
 brew update
 
+brew tap homebrew/versions
+brew tap homebrew/binary
+brew tap caskroom/versions
+
+brew install caskroom/cask/brew-cask
 brew install git
 brew install wget
 brew install curl
 brew install openssl
 brew install mysql
 brew install ant
+brew install bash-completion
 
+
+brew cask install skype
+brew cask install dropbox
+brew cask install skitch
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install boot2docker
+brew cask install slack
 
 #remove outdated versions
 brew cleanup
