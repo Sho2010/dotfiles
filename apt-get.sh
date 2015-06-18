bc602aae8b629b4bb0ebb00be4df9c9a487a91df
Sho2010 <sho20100@gmail.com>
2015-06-19 03:04:49 +0900

    add apt-get.sh

---
+++ apt-get.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+sudo apt-get install -y \
+  vim \
+  nkf \
+  wget \
+  irb \
+  libopenssl-ruby \
+  libreadline-ruby \
+  build-essential \
+  openssl \
+  libreadline5 \
+  libreadline-dev \
+  curl \
+  libssl-dev \
+  libxml2-dev \
+  libxslt-dev \
+  git-core \
+  zsh \
+  mysql-client \
+  tmux \
+;
