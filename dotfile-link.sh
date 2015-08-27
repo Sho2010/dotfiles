e1fc03fc81b1cfb19e957870b5f6ce1e8c982960
sho naito <sho20100@gmail.com>
2015-08-27 09:46:04 +0000

    add vimproc make script

--- dotfile-link.sh
+++ dotfile-link.sh
@@ -3,5 +3,10 @@
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/dot.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/dot.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/vimrcs ~/.vim/vimrcs
+ln -s ~/dotfiles/dot.gitignore.global ~/.gitignore.global
+ln -s ~/dotfiles/.vimrc ~/.vimrc
 
+if [ ! -d "~/.vim" ] ; then 
+  mkdir "~/.vim"
+fi
+ln -s ~/dotfiles/vimrcs ~/.vim/vimrcs
