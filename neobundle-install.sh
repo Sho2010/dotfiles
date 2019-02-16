e4133397f660df26b9f1337352e1d4faea4a8422
Sho2010 <sho20100@gmail.com>
2019-02-17 06:02:00 +0900

    uninstall neobundle

--- neobundle-install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-git clone https://github.com/Shougo/vimproc ~/.vim/bundle/vimproc
-
