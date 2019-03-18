3c227afdddb2dccd6c4d7a20f076809e8841eadf
sho naito <sho20100@gmail.com>
2019-03-19 00:46:51 +0900

    Support linux

--- brew.sh
+++ brew.sh
@@ -3,11 +3,6 @@
 # Make sure using latest Homebrew
 brew update
 
-brew tap homebrew/versions
-brew tap homebrew/binary
-brew tap caskroom/versions
-brew tap caskroom/cask
-
 brew install ant
 brew install bash-completion
 brew install curl
@@ -19,23 +14,28 @@ brew install p7zip
 brew install peco
 brew install rbenv
 brew install readline
-brew install reattach-to-user-namespace
 brew install tmux
 brew install yarn
 brew install jq
 brew install lua
-brew install vim --with-python3 --with-lua
 
-brew install mysql
-brew install redis
+case "${OSTYPE}" in
+darwin*)
+  # only mac
+  brew install reattach-to-user-namespace
+  brew install vim --with-python3 --with-lua
 
-brew install caskroom/cask/dropbox
-brew install caskroom/cask/skitch
-brew install caskroom/cask/slack
-brew install caskroom/cask/limechat
-brew install caskroom/cask/licecap
-brew install caskroom/cask/sequel-pro
-brew install caskroom/cask/discord
+  brew install caskroom/cask/dropbox
+  brew install caskroom/cask/skitch
+  brew install caskroom/cask/slack
+  brew install caskroom/cask/limechat
+  brew install caskroom/cask/licecap
+  brew install caskroom/cask/sequel-pro
+  brew install caskroom/cask/discord
+  ;;
+linux*)
+  ;;
+esac
 
 # js lint tools
 yarn global add prettier
