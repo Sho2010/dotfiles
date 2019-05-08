090f3b1d84606a936db495b92e55f51b2f67af4d
sho naito <sho20100@gmail.com>
2019-05-08 12:00:02 +0900

    Update install packages

--- package_installer/brew.sh
+++ package_installer/brew.sh
@@ -8,17 +8,30 @@ brew install bash-completion
 brew install curl
 brew install direnv
 brew install git
+brew install go
+brew install hub
+brew install jq
 brew install jsonlint
+brew install lua
 brew install openssl
 brew install p7zip
 brew install peco
 brew install rbenv
 brew install readline
+brew install tig
 brew install tmux
 brew install tree
 brew install yarn
-brew install jq
-brew install lua
+
+# k8s
+brew install kustomize
+brew install kubernetes-helm
+go get -u github.com/kubernetes-sigs/aws-iam-authenticator/cmd/aws-iam-authenticator
+
+# optional
+brew install kubectx
+brew install stern
+brew install kube-ps1
 
 case "${OSTYPE}" in
 darwin*)
@@ -29,10 +42,10 @@ darwin*)
   brew install caskroom/cask/dropbox
   brew install caskroom/cask/skitch
   brew install caskroom/cask/slack
-  brew install caskroom/cask/limechat
   brew install caskroom/cask/licecap
   brew install caskroom/cask/sequel-pro
   brew install caskroom/cask/discord
+  brew install caskroom/cask/charles
   ;;
 linux*)
   ;;
