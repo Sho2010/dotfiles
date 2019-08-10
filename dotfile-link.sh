250b3fa032f4a6fc9f44a9aba03c352dba8eabdd
Sho2010 <sho20100@gmail.com>
2019-08-11 05:41:31 +0900

    Add dein plugins settings

--- dotfile-link.sh
+++ dotfile-link.sh
@@ -14,3 +14,8 @@ ln -s ~/dotfiles/dot.pryrc ~/.pryrc
 ln -s ~/dotfiles/dot.eslintrc ~/.eslintrc
 ln -s ~/dotfiles/dot.ideavimrc ~/.ideavimrc
 ln -s ~/dotfiles/dot.tigrc ~/.tigrc
+
+mkdir -p ~/.config/dein
+ln -s ~/dotfiles/dein/go_lazy.toml ~/.config/dein/go_lazy.toml
+ln -s ~/dotfiles/dein/lazy.toml    ~/.config/dein/lazy.toml
+ln -s ~/dotfiles/dein/dein.toml    ~/.config/dein/dein.toml
