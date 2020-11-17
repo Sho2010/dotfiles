43e2486b2e8e00807adabdd0b8c3c728aeb7b42d
sho naito <sho20100@gmail.com>
2020-11-18 01:25:17 +0900

    Add go dev tools

---
+++ package_installer/go.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+go get github.com/golang/lint/golint
+go get github.com/nsf/gocode
+go get github.com/rakyll/gotest
+go get github.com/rogpeppe/godef
+go get golang.org/x/tools/cmd/goimports
+go get golang.org/x/tools/cmd/guru
+go get golang.org/x/tools/gopls
