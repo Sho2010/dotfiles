a525bb648c3ba2d9433532f26c21d9dd81eb5d13
sho naito <sho20100@gmail.com>
2022-09-10 11:58:52 +0900

    go get instead of go install

--- package_installer/go.sh
+++ package_installer/go.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-go get github.com/golang/lint/golint
-go get github.com/rakyll/gotest
-go get github.com/rogpeppe/godef
-go get golang.org/x/tools/cmd/goimports
-go get golang.org/x/tools/cmd/guru
-go get golang.org/x/tools/gopls
+go install github.com/golang/lint/golint@latest
+go install github.com/rakyll/gotest@latest
+go install github.com/rogpeppe/godef@latest
+go install golang.org/x/tools/cmd/goimports@latest
+go install golang.org/x/tools/cmd/guru@latest
+go install golang.org/x/tools/gopls@latest
