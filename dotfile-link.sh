fa9aac32b2a1cb3903ab8f154d80d8dafcb3722a
sho naito <sho20100@gmail.com>
2024-03-24 12:30:20 +0900

    Update setup script

--- dotfile-link.sh
+++ dotfile-link.sh
@@ -1,25 +1,34 @@
 #!/bin/sh
 
+
+if [ -e ~/.bashrc ]; then
+  echo ".bashrc exists. backup to .bashrc.bak"
+  mv ~/.bashrc ~/.bashrc.bak
+fi
 ln -s ~/dotfiles/.bashrc ~/.bashrc
+
+if [ -e ~/.zshrc ]; then
+  echo ".zshrc exists. backup to .zshrc.bak"
+  mv ~/.zshrc ~/.zshrc.bak
+fi
 ln -s ~/dotfiles/dot.zshrc ~/.zshrc
+
 touch ~/.zshrc.local
 
 ln -s ~/dotfiles/dot.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/dot.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/dot.gitignore.global ~/.gitignore.global
 
-ln -s ~/dotfiles/dot.vim ~/.vim
-ln -s ~/dotfiles/dot.vim/init.vim .vimrc
 ln -s ~/dotfiles/dot.pryrc ~/.pryrc
 ln -s ~/dotfiles/dot.eslintrc ~/.eslintrc
 ln -s ~/dotfiles/dot.ideavimrc ~/.ideavimrc
 ln -s ~/dotfiles/dot.tigrc ~/.tigrc
 
-mkdir -p ~/.config/dein
-ln -s ~/dotfiles/dein/go_lazy.toml ~/.config/dein/go_lazy.toml
-ln -s ~/dotfiles/dein/lazy.toml    ~/.config/dein/lazy.toml
-ln -s ~/dotfiles/dein/dein.toml    ~/.config/dein/dein.toml
+# mkdir -p ~/.config/dein
+# ln -s ~/dotfiles/dein/go_lazy.toml ~/.config/dein/go_lazy.toml
+# ln -s ~/dotfiles/dein/lazy.toml    ~/.config/dein/lazy.toml
+# ln -s ~/dotfiles/dein/dein.toml    ~/.config/dein/dein.toml
 
-mkdir -p ~/.config/nvim
+# Setup neovim
 ln -s ~/dotfiles/dot.vim ~/.config/nvim
 
