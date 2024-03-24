7d9fdb837a81f00391d84759b308932c71cbf788
sho naito <sho20100@gmail.com>
2024-03-25 07:21:08 +0900

    keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,4 +1,8 @@
-if vim.g.vscode then
+-- nvim, vscode 両方で有効化したいキーマップ
+-- ...
+
+
+if vim.g.vscode == 1 then
   require("keymaps_vscode")
 else
   require("keymaps_vim")
--- nvim/lua/keymaps_vscode.lua
+++ nvim/lua/keymaps_vscode.lua
@@ -1,6 +1,3 @@
-if vim.g.vscode then
-  require("keymaps_vscode")
-else
-  require("keymaps_vim")
-end
+-- VSCodeNotify() 関数によってVSCode側のコマンドを呼び出すことができます。
+vim.keymap.set("n", "<leader>n", "<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>")
 
