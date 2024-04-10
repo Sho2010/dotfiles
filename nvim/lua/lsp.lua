4486fc7437388ed66f858141660ba4561893c9ea
sho naito <sho20100@gmail.com>
2024-04-10 12:59:16 +0900

    lsp

---
+++ nvim/lua/lsp.lua
@@ -0,0 +1,39 @@
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = "rounded",
+})
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+  border = "rounded",
+  focusable = false,
+})
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+  vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false })
+
+vim.diagnostic.config({
+  update_in_insert = true,
+  underline = true,
+  severity_sort = true,
+  signs = {
+    text = { "", "", "", "" },
+    numhl = {
+      "DiagnosticSignError",
+      "DiagnosticSignWarn",
+      "DiagnosticSignInfo",
+      "DiagnosticSignHint",
+    },
+    linehl = {
+      "DiagnosticSignError",
+      "DiagnosticSignWarn",
+      "DiagnosticSignInfo",
+      "DiagnosticSignHint",
+    },
+  },
+  float = {
+    focusable = true,
+    style = "minimal",
+    border = "rounded",
+    source = "always",
+    header = "",
+    prefix = "",
+  },
+})
+
---
+++ package_installer/lsp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pip install -U setuptools
+pip install python-lsp-server
+pip install cmake-language-server
+
+npm i -g vscode-langservers-extracted
+npm i -g bash-language-server
+npm i -g typescript-language-server typescript
