3ae634d85ec142947ed120a159389f509d431e2d
sho naito <sho20100@gmail.com>
2024-06-05 16:20:53 +0900

    Add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
