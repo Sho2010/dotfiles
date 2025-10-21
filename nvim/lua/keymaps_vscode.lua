0ae5a29f694e28d843ac9c593042f801ca776db5
sho naito <sho20100@gmail.com>
2025-10-21 09:18:02 +0900

    update vscode neovim

--- nvim/lua/keymaps_vscode.lua
+++ nvim/lua/keymaps_vscode.lua
@@ -4,11 +4,14 @@
 
 -- できればファイルタイプに依存しないサブコマンドを作っておいてそれを呼ぶようにしたい
 vim.keymap.set("n", "<leader>a", "<Cmd>lua require('vscode-neovim').call('go.toggle.test.file')<CR>") -- -- toggle test <> code
+vim.keymap.set("n", "<leader>tt", "<Cmd>lua require('vscode-neovim').call('go.test.cursorOrPrevious')<CR>") -- -- toggle test <> code
 
 vim.keymap.set("n", "<leader>n", "<Cmd>lua require('vscode-neovim').call('workbench.explorer.fileView.focus')<CR>")
 vim.keymap.set("n", "<leader>uu", "<Cmd>lua require('vscode-neovim').call('workbench.action.quickOpen')<CR>")
-vim.keymap.set("n", "<leader>q", "<Cmd>lua require('vscode-neovim').call('editor.action.quickFix')<CR>")
+vim.keymap.set("n", "<leader>q", "<Cmd>lua require('vscode-neovim').call('problems.action.showQuickFixes')<CR>") -- quick fix
 vim.keymap.set("n", "<leader>f", "<Cmd>lua require('vscode-neovim').call('editor.action.formatDocument')<CR>")
+vim.keymap.set("n", "<leader>d", "<Cmd>lua require('vscode-neovim').call('editor.action.marker.next')<CR>") -- 次の問題 (エラー、警告、情報) へ移動 vim.diagnostic.goto_next
+vim.keymap.set("n", "<leader>c", "<Cmd>lua require('vscode-neovim').call('editor.action.inlineSuggest.trigger')<CR>") -- 次の問題 (エラー、警告、情報) へ移動 vim.diagnostic.goto_next
 
 vim.keymap.set("n", "<C-o>", "<Cmd>lua require('vscode-neovim').call('workbench.action.navigateBack')<CR>") -- 前のファイルに移動
 vim.keymap.set("n", "<C-]>", "<Cmd>lua require('vscode-neovim').call('editor.action.revealDefinition')<CR>") -- 定義ジャンプ
@@ -17,7 +20,11 @@ vim.keymap.set("n", "<C-]>", "<Cmd>lua require('vscode-neovim').call('editor.act
 vim.keymap.set("n", "dp", "<Cmd>lua require('vscode-neovim').call('editor.action.marker.prev')<CR>") -- 前の問題 (エラー、警告、情報) へ移動 vim.diagnostic.goto_prev
 vim.keymap.set("n", "dn", "<Cmd>lua require('vscode-neovim').call('editor.action.marker.next')<CR>") -- 次の問題 (エラー、警告、情報) へ移動 vim.diagnostic.goto_next
 
--- TODO: 
+-- [C]hat
+-- vim.keymap.set("n", "ci", "<Cmd>lua require('vscode-neovim').call('')<CR>") --
+vim.keymap.set("n", "c_", "<Cmd>lua require('vscode-neovim').call('editor.action.inlineSuggest.trigger')<CR>") -- インライン候補をトリガーする '/' は '_'で設定
+
+-- TODO:
 -- MarkerNavigationがめちゃくちゃ鬱陶しいのでこれを組み込みたい closeMarkersNavigation
 -- 動かなかった原因としてはcloseMarkersNavigationが呼び出されるとこまではうまくいったが、NEXTを繰り返すとループしてくれない
 -- vim.keymap.set("n", "dn", "<Cmd>lua require('vscode-neovim').call('editor.action.marker.next')<CR><Cmd>lua require('vscode-neovim').call('closeMarkersNavigation')<CR>") -- 次の問題 (エラー、警告、情報) へ移動 vim.diagnostic.goto_next
@@ -29,10 +36,12 @@ vim.keymap.set("n", "eo", "<Cmd>lua require('vscode-neovim').call('workbench.pan
 vim.keymap.set("n", "et", "<Cmd>lua require('vscode-neovim').call('workbench.view.testing.focus')<CR>")           -- テスト
 vim.keymap.set("n", "ed", "<Cmd>lua require('vscode-neovim').call('workbench.debug.action.focusRepl')<CR>")       -- デバッグ
 
+-- TODO: filetypeのチェック仕方がわからない
 -- if filetype go
 if vim.bo.filetype == "go" then
   -- vim.keymap.set("n", "<leader>rr", "<Cmd>call VSCodeNotify('editor.action.rename')<CR>") -- リネーム
   -- vim.keymap.set("n", "<leader>gg", "<Cmd>call VSCodeNotify('editor.action.goToImplementation')<CR>") -- 実装ジャンプ
 end
+
 
 
